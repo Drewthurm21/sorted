3c451fba2e66dd597e936082f922bf38c162f03c
Drewthurm21 <drewthurm21@gmail.com>
2023-09-07 16:54:50 -0500

    started navbar structure

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,7 +1,49 @@
+import { AiOutlineBorderlessTable } from "react-icons/ai";
+import { ReactElement } from "react";
+
 export default function NavbarWrapper() {
   return (
     <div className="absolute top-0 w-full h-20 justify-between align-center bg-emerald-400">
-      navbar
+      <Navbar />
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <div className="flex h-full justify-between">
+      <div className="flex w-60 justify-evenly items-center">Algo Vis_</div>
+      <div className="flex w-1/2 mr-20 justify-evenly items-center">
+        {navbarItems.map((item) => (
+          <NavbarItem {...item} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const navbarItems = [
+  { icon: <AiOutlineBorderlessTable />, label: "label" },
+  { icon: <AiOutlineBorderlessTable />, label: "label" },
+  { icon: <AiOutlineBorderlessTable />, label: "label" },
+];
+
+type NavbarItemProps = {
+  icon: ReactElement;
+  label: String;
+};
+
+function NavbarItem({ icon, label }: NavbarItemProps) {
+  return (
+    <div>
+      {icon ? (
+        <div>
+          {icon}
+          <div>{label}</div>
+        </div>
+      ) : (
+        <div>{label}</div>
+      )}
     </div>
   );
 }
