9ffa3ba0817cfe0b0cd0b15009aad1ee7a759622
Drewthurm21 <drewthurm21@gmail.com>
2023-09-08 13:38:37 -0500

    added bubbleSort - cleaned up code & variable names

--- src/components/ArrayGenerator/ArrayGenerator.tsx
+++ src/components/ArrayGenerator/ArrayGenerator.tsx
@@ -2,18 +2,34 @@
 
 import { useEffect, useState } from "react";
 
-function generateRandomNumber(max: number) {
+function generateRandomNumber(max: number): number {
   return Math.floor(Math.random() * max) + 1;
 }
 
-function generateNewRandomArray(length: number) {
-  let newArr = new Array(length).fill(0);
-  return newArr.map((el) => generateRandomNumber(500));
+function generateNewRandomArray(length: number): number[] {
+  return Array.from({ length }, () => generateRandomNumber(700));
+}
+
+function bubbleSort(arr: number[]): number[] {
+  const arrayCopy = [...arr];
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arrayCopy.length - 1; i++) {
+      if (arrayCopy[i] > arrayCopy[i + 1]) {
+        const temp = arrayCopy[i];
+        arrayCopy[i] = arrayCopy[i + 1];
+        arrayCopy[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arrayCopy;
 }
 
 export default function ArrayGeneratorWrapper() {
-  const newRandomArray = generateNewRandomArray(20);
-  const [sortingArray, setSortingArray] = useState<number[]>(newRandomArray);
+  const initialArray = generateNewRandomArray(20);
+  const [sortingArray, setSortingArray] = useState<number[]>(initialArray);
 
   return (
     <div className="flex gap-4 h-1/2 w-full justify-center items-end">
