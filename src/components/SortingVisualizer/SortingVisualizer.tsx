30f1794a36a19a838f6a0cd52b19a3e06d05d41b
Drewthurm21 <drewthurm21@gmail.com>
2023-09-20 15:10:27 -0500

    refactored bubblesort

--- src/components/SortingVisualizer/SortingVisualizer.tsx
+++ src/components/SortingVisualizer/SortingVisualizer.tsx
@@ -1,66 +1,78 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { generateBubbleSortAnimations } from "./SortingAlgorithms";
 
 function generateRandomNumber(max: number): number {
   return Math.floor(Math.random() * max) + 1;
 }
 
 function generateNewRandomArray(length: number): number[] {
-  return Array.from({ length }, () => generateRandomNumber(700));
+  return Array.from({ length }, () => generateRandomNumber(500));
 }
 
-function bubbleSort(arr: number[]): number[] {
-  const arrayCopy = [...arr];
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < arrayCopy.length - 1; i++) {
-      if (arrayCopy[i] > arrayCopy[i + 1]) {
-        const temp = arrayCopy[i];
-        arrayCopy[i] = arrayCopy[i + 1];
-        arrayCopy[i + 1] = temp;
-        swapped = true;
-      }
-    }
-  } while (swapped);
-  return arrayCopy;
-}
+const handleFrame = (frameNumber: number, col1: any, col2: any) => {
+  let c1h = col1.style.height;
+  let c2h = col2.style.height;
+  let el1 = col1.firstChild;
+  let el2 = col2.firstChild;
+  let text = el1.firstChild.innerText;
+
+  if (frameNumber % 3 === 1) {
+    el1.style.backgroundColor = "red";
+    el2.style.backgroundColor = "purple";
+  } else if (frameNumber % 3 === 2) {
+    el1.style.backgroundColor = "green";
+    el2.style.backgroundColor = "green";
+    el1.firstChild.innerText = el2.firstChild.innerText;
+    el2.firstChild.innerText = text;
+    col1.style.height = c2h;
+    col2.style.height = c1h;
+  } else {
+    el1.style.backgroundColor = "blue";
+    el2.style.backgroundColor = "blue";
+  }
+};
 
 export default function SortingVisualizer() {
-  const initialArray = generateNewRandomArray(40);
-  const [sortingArray, setSortingArray] = useState<number[]>(initialArray);
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [sortingInProgress, setSortingInProgress] = useState<boolean>(false);
+  const [sortingArray, setSortingArray] = useState<number[]>(
+    generateNewRandomArray(20)
+  );
 
   useEffect(() => {
     if (!sortingInProgress) return;
-    if (currentIndex < sortingArray.length) {
-      const timer = setTimeout(() => {
-        const newArray: number[] = bubbleSort(
-          sortingArray.slice(0, currentIndex + 1)
-        );
-        setSortingArray(newArray.concat(sortingArray.slice(currentIndex + 1)));
-        setCurrentIndex(currentIndex + 1);
-      }, 250);
-      return () => clearTimeout(timer);
+    const animationFrames = generateBubbleSortAnimations(sortingArray);
+    for (let i = 0; i < animationFrames.length; i++) {
+      setTimeout(() => {
+        const arrayElements = document.querySelectorAll(".array-element-node");
+        const [pos1, pos2] = animationFrames[i];
+        const col1 = arrayElements[pos1];
+        const col2 = arrayElements[pos2];
+        for (let idx = 1; idx <= 3; idx++) {
+          setTimeout(() => {
+            handleFrame(idx, col1, col2);
+          }, idx * 15);
+        }
+      }, i * 45);
     }
-  }, [currentIndex, sortingArray, sortingInProgress]);
+  }, [sortingInProgress]);
 
   return (
     <>
       <div className="flex gap-4 h-1/2 w-full justify-center items-end">
-        {sortingArray.map((el: number, idx: number, arr: number[]) => {
-          let color;
-
+        {sortingArray.map((value: number, idx: number, arr: number[]) => {
           return (
             <div
-              style={{ height: el, display: "flex", flexDirection: "column" }}
+              className="array-element-node"
+              style={{
+                height: value,
+              }}
             >
               <ArrayElementNode
                 key={idx}
-                label={arr.length < 30 ? `${el}` : ""}
-                color={idx === currentIndex ? "bg-emerald-500" : "bg-blue-500"}
+                label={arr.length < 30 ? `${value}` : ""}
+                color={"bg-blue-500"}
               />
             </div>
           );
@@ -71,7 +83,6 @@ export default function SortingVisualizer() {
         onClick={() => {
           const newArray = generateNewRandomArray(20);
           setSortingArray(newArray);
-          setCurrentIndex(0);
           setSortingInProgress((prev) => false);
         }}
       >
@@ -91,7 +102,7 @@ export default function SortingVisualizer() {
 function ArrayElementNode({ color, label }: { color: string; label: string }) {
   return (
     <div
-      className={`flex h-full w-6 justify-center items-end text-xs ${color}`}
+      className={`flex h-full w-3 justify-center items-end text-xs ${color}`}
     >
       <div className="translate-y-6 -rotate-45">{label}</div>
     </div>
